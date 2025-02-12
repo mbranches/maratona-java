4149ea0da3370337ba20c1b6f391621acca75c6d
Marcus Branches <marcus.branches@icloud.com>
2025-02-12 11:34:07 -0300

    feat: adicionando a aula de BufferedReader

--- src/academy/devdojo/maratona/files/BufferedWriter01.java
+++ src/academy/devdojo/maratona/files/BufferedWriter01.java
@@ -8,12 +8,11 @@ public class BufferedWriter01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fwriter = new FileWriter(file, true);
-             BufferedWriter bfWriter = new BufferedWriter(fwriter)) {
-
+             BufferedWriter bfWriter = new BufferedWriter(fwriter)
+        ) {
             bfWriter.write("Utilizando Buffered Writer...");
             bfWriter.newLine();
             bfWriter.flush();  //esvazia o writer
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
