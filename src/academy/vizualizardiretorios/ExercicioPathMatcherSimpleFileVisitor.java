dceb30c3480cda529215366e587e82ca032ad602
mbranches <marcusvbranches@gmail.com>
2025-02-14 17:02:38 -0300

    feat: adicionando um pequeno exercício de PathMatcher e SimpleFileVisitor

---
+++ src/academy/vizualizardiretorios/ExercicioPathMatcherSimpleFileVisitor.java
@@ -0,0 +1,24 @@
+package academy.vizualizardiretorios;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class ListAllFile extends SimpleFileVisitor<Path> {
+    PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*[tT]est*.{java,class}");
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (matcher.matches(file)) System.out.println(file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+public class ExercicioPathMatcherSimpleFileVisitor {
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+
+        Files.walkFileTree(root, new ListAllFile());
+    }
+
+}
