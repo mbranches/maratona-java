00ea8a0c9c318961dca7907942e49652c52693c9
mbranches <marcusvbranches@gmail.com>
2025-02-14 15:00:32 -0300

    feat: adicionando a aula de DirectoryStream

---
+++ src/academy/DirectoryStream/DirectoryStream01.java
@@ -0,0 +1,22 @@
+package academy.DirectoryStream;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirectoryStream01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("./");
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for (Path path : stream) {
+                DirectoryStream<Path> stream2 = Files.newDirectoryStream(path);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
