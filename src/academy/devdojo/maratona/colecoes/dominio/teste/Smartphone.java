54670c60c4acd203d7bdc3a044cb3c5449256d85
mbranches <marcusvbranches@gmail.com>
2025-02-16 13:35:26 -0300

    feat: adicionando a aula02 de equals em coleções

--- src/academy/devdojo/maratona/colecoes/dominio/teste/Smartphone.java
+++ src/academy/devdojo/maratona/colecoes/dominio/teste/Smartphone.java
@@ -9,6 +9,24 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    //Reflexivo: x.equals(x) == true para tudo que for diferente de null
+    //Simétrico: para x e y diferente de null, se x.equals(y) == true logo y.equals(x) == true
+    //Transitividade: se x.equals(y) == true, e x.equals(z) == true, logo: y.equals(z) == true
+    //Consistente: x.equals(x) sempre is true, se x != null
+    //para x != null, x.equals(null) == false
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+
+        if (this == obj) return true;
+
+        if (this.getClass() != obj.getClass()) return false;
+
+        Smartphone smartphone = (Smartphone) obj;
+
+        return this.serialNumber != null && this.serialNumber.equals(smartphone.getSerialNumber());
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
--- src/academy/devdojo/maratona/colecoes/teste/EqualsTest01.java
+++ src/academy/devdojo/maratona/colecoes/teste/EqualsTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratona.colecoes.dominio.teste.Smartphone;
 public class EqualsTest01 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC2", "Iphone");
-        Smartphone s2 = new Smartphone("1ABC2", "Iphone");
+        Smartphone s2 = new Smartphone("1ABC2", "Ip");
 
         System.out.println(s1.equals(s2)); //por padrão o equals compara a posição em memória
     }
