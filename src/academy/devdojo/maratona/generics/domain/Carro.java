c93d919a371d810f0a5133a968d458e89088fb2d
Marcus Branches <marcus.branches@icloud.com>
2025-02-21 16:17:10 -0300

    feat: aula 04 de generics pt2

--- src/academy/devdojo/maratona/generics/domain/Carro.java
+++ src/academy/devdojo/maratona/generics/domain/Carro.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratona.generics.domain;
 
 public class Carro {
+    private String nome;
+
+    public Carro(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
--- src/academy/devdojo/maratona/generics/service/CarroRentavelService.java
+++ src/academy/devdojo/maratona/generics/service/CarroRentavelService.java
@@ -1,4 +1,24 @@
 package academy.devdojo.maratona.generics.service;
 
+import academy.devdojo.maratona.generics.domain.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarroRentavelService {
+    private List<Carro> consultaCarrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Fusca")));
+
+    public Carro buscarCarroDisponivel() {
+        System.out.println("Buscando carro disponivel");
+        Carro carro = consultaCarrosDisponiveis.remove(0);
+        System.out.println("Alugando carro: " + carro);
+        System.out.println("Carros disponiveis para alugar: " + consultaCarrosDisponiveis);
+        return carro;
+    }
+
+    public void retornarCarroAlugado(Carro carro) {
+        System.out.println("Devolvendo carro: " + carro);
+        consultaCarrosDisponiveis.add(carro);
+        System.out.println("Carros disponiveis para alugar: " + consultaCarrosDisponiveis);
+    }
 }
