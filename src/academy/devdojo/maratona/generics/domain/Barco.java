0ed6132a9943564bf66114f458f6376210268fb0
mbranches <marcusvbranches@gmail.com>
2025-02-21 16:17:10 -0300

    feat: aula 04 de generics pt2

--- src/academy/devdojo/maratona/generics/domain/Barco.java
+++ src/academy/devdojo/maratona/generics/domain/Barco.java
@@ -1,4 +1,16 @@
 package academy.devdojo.maratona.generics.domain;
 
 public class Barco {
+    private String nome;
+
+    public Barco(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Barco{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
--- src/academy/devdojo/maratona/generics/test/ClasseGenericaTest01.java
+++ src/academy/devdojo/maratona/generics/test/ClasseGenericaTest01.java
@@ -1,4 +1,14 @@
 package academy.devdojo.maratona.generics.test;
 
-public class ClasseGenericaTest01 {
+import academy.devdojo.maratona.generics.domain.Carro;
+import academy.devdojo.maratona.generics.service.CarroRentavelService;
+
+public class ClasseGenericaTest01{
+    public static void main(String[] args) {
+        CarroRentavelService carroRentavelService = new CarroRentavelService();
+
+        Carro carro = carroRentavelService.buscarCarroDisponivel();
+        System.out.println("Utilizando carro p 1 mes....");
+        carroRentavelService.retornarCarroAlugado(carro);
+    }
 }
