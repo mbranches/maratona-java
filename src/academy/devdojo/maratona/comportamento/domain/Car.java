9017c2becdf8f01a4da68aa69c71a9b5b5baab91
Marcus Branches <marcus.branches@icloud.com>
2025-02-22 12:21:31 -0300

    feat: aula01 de comportamentos

---
+++ src/academy/devdojo/maratona/comportamento/domain/Car.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratona.comportamento.domain;
+
+public class Car {
+    private String name = "Audi";
+    private String color;
+    private int year;
+
+    public Car(String color, int year) {
+        this.color = color;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
