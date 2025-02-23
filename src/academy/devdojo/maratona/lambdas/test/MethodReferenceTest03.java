2d305a77d291022c9dbc59d002408e4d4aaa5954
Marcus Branches <marcus.branches@icloud.com>
2025-02-23 11:38:04 -0300

    feat: aula 02 de method reference - Ref a métodos não estatico pela classe ou instancia

---
+++ src/academy/devdojo/maratona/lambdas/test/MethodReferenceTest03.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratona.lambdas.test;
+
+import academy.devdojo.maratona.lambdas.domain.Anime;
+import academy.devdojo.maratona.lambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Refence to an instance method of a particular object
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animes = new ArrayList<>(List.of(new Anime("Naruto", 210), new Anime("OnePiece", 900), new Anime("Dragon Ball", 211)));
+//        animes.sort((o1, o2) -> animeComparators.compareByEpisodesNonStatic(o2, o2));
+        animes.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animes);
+    }
+}
