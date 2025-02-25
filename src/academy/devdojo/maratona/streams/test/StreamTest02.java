2f155245d61e0b6bd2541072eb3381acaacca760
Marcus Branches <marcus.branches@icloud.com>
2025-02-25 16:32:25 -0300

    feat: aula 02 de Stream - Introdução 02

--- src/academy/devdojo/maratona/streams/test/StreamTest02.java
+++ src/academy/devdojo/maratona/streams/test/StreamTest02.java
@@ -5,10 +5,11 @@ import academy.devdojo.maratona.streams.domain.LightNovel;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //1. Order Light Novel by title
 //2. Retrieve the first 3 titles Light Novels with price less than 4
-public class StreamTest01 {
+public class StreamTest02 {
     private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
             new LightNovel("Naruto", 8.99),
             new LightNovel("Overlord", 3.99),
@@ -20,18 +21,13 @@ public class StreamTest01 {
     ));
 
     public static void main(String[] args) {
-        lightNovels.sort(Comparator.comparing(LightNovel::getTitle));
+        List<String> lightNovelsFiltered = lightNovels.stream()
+                .sorted(Comparator.comparing(LightNovel::getTitle))
+                .filter(ln -> ln.getPrice() <= 4)
+                .limit(3)
+                .map(LightNovel::getTitle)
+                .collect(Collectors.toList());
 
-        List<String> titles = new ArrayList<>();
-
-        for (LightNovel lightNovel : lightNovels) {
-            if (lightNovel.getPrice() <= 4) {
-                titles.add(lightNovel.getTitle());
-            }
-            if (titles.size() > 2) break;
-        }
-
-        System.out.println(lightNovels);
-        System.out.println(titles);
+        System.out.println(lightNovelsFiltered);
     }
 }
